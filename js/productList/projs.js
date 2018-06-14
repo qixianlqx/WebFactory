d045508286f54a33e0bfb259a2dee3116a57235e
qixianlu <qixian_lqx@163.com>
2018-06-14 21:58:21 +0800

    修改工程案例页
    Signed-off-by: qixianlu <qixian_lqx@163.com>

--- js/productList/projs.js
+++ js/productList/projs.js
@@ -7,11 +7,12 @@ function infoList(id, title, img, apptime) {
     self.apptime = ko.observable(apptime);
 }
 //分类
-function TypeList(id, name, infos) {
+function TypeList(id, name, className, infos) {
     var self = this;
     self.id = ko.observable(id);
     self.name = ko.observable(name);
     self.infos = ko.observableArray(infos || []);
+    self.className = ko.observable(className);
 }
 
 function setItem(infos) {
@@ -30,19 +31,21 @@ function ViewModel() {
     var self = this;
 
     self.TypeList = ko.observableArray([
-        new TypeList(1, '石油化工', [new infoList(1, '天津LNG接收站', 'sy_tianjing.jpg')]),
-        new TypeList(2, '风电能源', [new infoList(1, '三峡新能源公司', 'fn_sanxia.jpg'), new infoList(2, '国家电投集团', 'fn_guojia.jpg')]),
-        new TypeList(3, '钢铁行业', [new infoList(1, '潍坊特钢集团有限公司', 'gt_tangfag.jpg'), new infoList(2, '襄汾星原钢铁集团', 'gt_xiangfen.jpg')]),
-        new TypeList(4, '建筑水泥', [new infoList(1, '赞皇金隅水泥有限公司', 'sl_zanhuang.jpg'), new infoList(2, '叶城天山水泥有限责任公司', 'sl_yecheng.jpg')]),
-        new TypeList(5, '变电站电力所', [new infoList(1, '迁安市九江线材有限责任公司', 'bdz_qianan.jpg'), new infoList(2, '京能中电电力工程有限公司', 'bdz_jingnong.jpg'), new infoList(3, "北京四方继保工程技术有限公司", 'bdz_sifang.jpg')])
+        new TypeList(1, '石油化工', 'am-icon-pie-chart', [new infoList(1, '天津LNG接收站', 'sy_tianjing.jpg')]),
+        new TypeList(2, '风电能源', 'am-icon-won', [new infoList(1, '三峡新能源公司', 'fn_sanxia.jpg'), new infoList(2, '国家电投集团', 'fn_guojia.jpg')]),
+        new TypeList(3, '钢铁行业', 'am-icon-gears', [new infoList(1, '潍坊特钢集团有限公司', 'gt_tangfag.jpg'), new infoList(2, '襄汾星原钢铁集团', 'gt_xiangfen.jpg')]),
+        new TypeList(4, '建筑水泥', 'am-icon-institution', [new infoList(1, '赞皇金隅水泥有限公司', 'sl_zanhuang.jpg'), new infoList(2, '叶城天山水泥有限责任公司', 'sl_yecheng.jpg')]),
+        new TypeList(5, '变电站电力所', 'am-icon-bolt', [new infoList(1, '迁安市九江线材有限责任公司', 'bdz_qianan.jpg'), new infoList(2, '京能中电电力工程有限公司', 'bdz_jingnong.jpg'), new infoList(3, "北京四方继保工程技术有限公司", 'bdz_sifang.jpg')])
     ]);
     self.infoList = ko.observableArray(setItem(self.TypeList()[0].infos()));
     self.typeClick = function(item) {
         self.count = item.infos().length;
         self.infoList(setItem(item.infos()));
+        self.className(item.className());
         self.currentName(item.name());
     }
     self.count = self.TypeList()[0].infos().length;
     self.currentName = ko.observable(self.TypeList()[0].name());
+    self.className = ko.observable(self.TypeList()[0].className());
 }
 ko.applyBindings(new ViewModel());
